37275b0a7ee01d30cf58b70b94dfa2a20a329721
Jacob Rosario <jacobrosario0225@gmail.com>
2025-11-16 09:25:25 -0800

    added mock data

--- frontend/components/DownloadButton.tsx
+++ frontend/components/DownloadButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
 
@@ -7,23 +9,30 @@ interface DownloadButtonProps {
 }
 
 export function DownloadButton({ type, disabled = false }: DownloadButtonProps) {
+  const fileMap = {
+    resume: "/resume-for-apple.pdf",
+    coverLetter: "/cover-letter-for-apple.pdf",
+  };
+  
   const line2 = type === "resume" ? "Resume" : "Cover Letter";
 
   return (
-    <Button
-      variant="outline"
-      disabled={disabled}
-      className={`w-48 h-48 rounded-3xl text-white flex flex-col items-center justify-center gap-4 ${
-        disabled
-          ? "cursor-not-allowed opacity-50"
-          : ""
-      }`}
+    <a
+    href={disabled ? undefined: fileMap[type]}
+    download
+    className={disabled ? "pointer-events-none opacity-50" : ""} 
     >
-      <DownloadIcon className="w-6! h-6!" />
-      <div className="flex flex-col items-center text-base font-normal">
-        <span>Download</span>
-        <span>{line2}</span>
-      </div>
-    </Button>
+      <Button
+        variant="outline"
+        disabled={disabled}
+          className={`w-48 h-48 rounded-3xl text-white flex flex-col items-center justify-center gap-4`}
+    >
+        <DownloadIcon className="w-6! h-6!" />
+        <div className="flex flex-col items-center text-base font-normal">
+          <span>Download</span>
+          <span>{line2}</span>
+        </div>
+      </Button>
+      </a>
   );
 }
