a96f7fed28a214f275b079dd8dec6b4b16d3f69b
Jacob Rosario <jacobrosario0225@gmail.com>
2025-11-15 23:53:53 -0800

    created button for pdf and link

---
+++ frontend/app/PDFUpload/PDFUpload.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+
+export function PDFUpload() {
+    const [file, setFile] = useState<File | null>(null);
+    const [error, setError] = useState("");
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const selectedFile = e.target.files?.[0];
+
+        // case where there's no file selected
+        if (!selectedFile) {
+            setFile(null);
+            setError("");
+            return;
+    
+        }
+
+        // case where the file type is NOT a .pdf
+        if (selectedFile.type !== "application/pdf") {
+            setFile(null);
+            setError("Upload a PDF file");
+            return;
+        }
+
+        // case where th efile is greater than 10MB in bytes
+        const maxSize = 10 * 1024 * 1024; 
+        if (selectedFile.size > maxSize) {
+            setFile(null);
+            setError("File size must be less than 10MB");
+            return;
+        }
+
+        // if all things check out, save the file
+        setFile(selectedFile);
+        setError('');
+
+    }
+
+    function handleButtonClick() {
+        fileInputRef.current?.click();
+    }
+
+    return(
+        <div> 
+            <input 
+            ref={fileInputRef}
+            type='file'
+            accept=".pdf, application/pdf"
+            onChange={handleFileChange}
+            className="hidden"/>
+
+            <Button>
+                Upload Resume
+            </Button>
+
+        </div>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import "./global.css";
 import { URLBar } from "./URLBar/URLBar";
+import { PDFUpload } from "./PDFUpload/PDFUpload";
 
 export default function Home() {
   return (
     <div>
       <URLBar />
+      <PDFUpload />
     </div>
   );
 }
